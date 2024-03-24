6769bcaeb4883eb47c4958b6c820582135e670e4
Hashiq-M <aa0265175@gmail.com>
2024-03-24 16:46:03 +0530

    first

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+
+let data_product = [
+  {
+    id: 1,
+    name: "Black Jerkin",
+    image: p1_img,
+    new_price: 800,
+    old_price: 1000,
+  },
+  {
+    id: 2,
+    name: "Pink t-shirt",
+    image: p2_img,
+    new_price: 350,
+    old_price: 500,
+  },
+  {
+    id: 3,
+    name: "Crop Top",
+    image: p3_img,
+    new_price: 312,
+    old_price: 400,
+  },
+  {
+    id: 4,
+    name: "Peplum Hem Blouse",
+    image: p4_img,
+    new_price: 425,
+    old_price: 650,
+  },
+];
+
+export default data_product;
